db1fc61dc97378d5cb53d71953845f07e21c42ae
shodmonovjavohir986-cmyk <shodmonovjavohir986@gmail.com>
2026-06-13 09:11:18 +0500

    Create config.py
    .

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN         = "8537787397:AAE_C9V9oXLsvLpYvmX4N06EX-oc6MPu5J4"
+ADMIN_IDS         = [7553944761,7787749469]          # Admin Telegram ID lari
+GROUP_ID          = -4993088456       # Guruh ID (yangi yozilishlar keladi)
+YANDEX_MAP_LINK   = "https://yandex.uz/maps/?feedback=map%2Fedit&feedback-context=user_profile.edit_map&l=sat%2Ccarparks&ll=68.073109%2C38.352709&z=19"
+PHONE_NUMBER      = "+998 88 188 77 20"
+CENTER_ADDRESS    = "Surxon daryo Uzun tumani  kichik  Jon cheka pavoroti  "
+CENTER_ABOUT      = "Grand Study — 2020-yildan faoliyat yurituvchi zamonaviy o'quv markazi."
+REFERRAL_BONUS    = 10000
+DB_PATH           = "grand_study.db"
+GIF_PATH          = "loading.gif"
+
+# Majburiy obuna
+TG_CHANNEL        = "@kerakli2121"
+TG_CHANNEL_LINK   = "https://t.me/kerakli2121"
+INSTAGRAM_LINK    = "https://www.instagram.com/grand_study_?igsh=MXM5aHllb3gwdHdvdA=="
+INSTAGRAM_NAME    = "@grand_study_"
